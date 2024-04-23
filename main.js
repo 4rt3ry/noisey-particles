749100f273aafb1d1efd99cc2020bf9e97d4a6f6
Nick Kannenberg (RIT Student) <nrk5987@rit.edu>
2024-04-23 12:17:14 -0400

    General particle fixes

--- main.js
+++ main.js
@@ -1,37 +1,48 @@
-import {Particle} from './particle.js';
+import { Particle } from './particle.js';
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+    let particles = [];
+
+    let pNum = 2;
+
+    for(let i = 0; i < pNum; i ++){
+        particles.push(new Particle(
+            {
+                size: 1,
+                desiredPitch: 1,
+                pitchVariance: 1,
+                volumeMultiplier: 1,
+                position: [100, 100],
+                wander: [1, 0],
+                color: [0, 0, 0],
+                energy: 0,
+                eDecay: 0,
+                eMultiplier: 1,
+                eThreshhold: 5,
+                isParent: true
+            }));
+    }
+    
+    let delta = 1.0/60.0;
+
+function draw() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.fillStyle = "white";
+    
+    for(let i = 0; i < particles.length; i ++){
+        particles[i].update(delta, ctx);
+        particles[i].inputSound({pitch: 1, volume: 1.5});
+    }
 
 
-function draw(){
-    let particle = new Particle(
-        {size:1, 
-            desiredPitch: 1, 
-            pitchVariance: 1,
-            volumeMultiplier: 1,
-            position: [1, 1],
-            wander: [1, 0],
-            color: [0, 0, 0],
-            energy: [0],
-            eDecay: [0.5],
-            eMultiplier: 1,
-            eThreshhold: 5,
-            isParent: true
-    });
 
-    //while(true){
-        ctx.fillStyle = "rgb(0,0,0)"
-        
-        // Test
-        ctx.arc(1.0, 1.0, 10, 0, Math.PI * 2, true);
-        console.log(particle);
-        let pos = particle.getPosition();
-        let size = particle.getSize();
-        console.log(pos);
-        ctx.arc(pos[0], pos[1], size, 0, Math.PI * 2, true);
-    //}
+    // Test
+
+    window.requestAnimationFrame(draw);
 }
 
 window.addEventListener("load", draw);
--- particle.js
+++ particle.js
@@ -1,115 +1,135 @@
 const sizeMult = 0.5; // Size multiplier based on energy
 const speed = 5.0; // Particle wander speed
 
-class Particle{    
-    constructor(vars){    
+class Particle {
+    constructor(vars) {
         let initialVariables = vars;
-        let size = vars.size || 1.0;
-        
-        let desiredPitch = vars.desiredPitch || 1000.0;
-        let pitchVariance = vars.pitchVariance || 200.0;
-    
-        let volumeMultiplier = vars.volumeMultiplier || 0.1;
-    
-        let position = vars.position || [0.0, 0.0];
-        let wander = vars.wander || [1.0, 0.0];
-    
-        let color = vars.color || [0, 0, 0];
-    
-        let energy = vars.energy || 0.0;
-        let eDecay = vars.eDecay || 1.0;
-        let eMultiplier = vars.eMultiplier || 1.0;
-        let eThreshhold = vars.eThreshhold || 5.0;
-        
+        let size = vars.size ?? 1.0;
+        let sizeMult = vars.sizeMult ?? 0.5;
+
+        let desiredPitch = vars.desiredPitch ?? 1000.0;
+        let pitchVariance = vars.pitchVariance ?? 200.0;
+
+        let volumeMultiplier = vars.volumeMultiplier ?? 0.1;
+
+        let position = vars.position ?? [0.0, 0.0];
+        let wander = vars.wander ?? [1.0, 0.0];
+
+        let color = vars.color ?? [0, 0, 0];
+
+        let energy = vars.energy ?? 0.0;
+        let eDecay = vars.eDecay ?? 1.0;
+        let eMultiplier = vars.eMultiplier ?? 1.0;
+        let eThreshhold = vars.eThreshhold ?? 5.0;
+
         // Only parents split, children die when splitting
-        let isParent = vars.isParent || true;
-        
-        let energyPerSecond = 0;
-        
-        let children = vars.children || [];
-        Object.assign(this, initialVariables, sizeMult, speed, size, desiredPitch, pitchVariance, volumeMultiplier, position, wander, color, energy, eDecay, eMultiplier, eThreshhold, isParent, energyPerSecond, children);
+        let isParent = vars.isParent ?? true;
+
+        let energyPerSecond = vars.energyPerSecond ?? 0;
+
+        let children = vars.children ?? [];
+
+        Object.assign(this, {
+            initialVariables,
+            sizeMult, speed,
+            size, desiredPitch,
+            pitchVariance, volumeMultiplier,
+            position, wander, color,
+            energy, eDecay, eMultiplier,
+            eThreshhold, isParent, energyPerSecond, children
+        });
     }
-    
-    update = (delta) => {
+
+    update = (delta, ctx) => {
         // Randomize + Normalize wander, move
-        this.wander[0] += (Math.random()-0.5) * 0.2;
-        this.wander[1] += (Math.random()-0.5) * 0.2;
-        normalizeWander();
-        moveForward();
-    
+        this.wander[0] += (Math.random() - 0.5) * 0.2;
+        this.wander[1] += (Math.random() - 0.5) * 0.2;
+        this.normalizeWander();
+        this.moveForward(delta);
+
         // Increment and decay energy
         this.energy += this.energyPerSecond * delta * this.eMultiplier;
-        this.energy -= this.eDecay * this.delta;
-    
+        this.energy -= this.eDecay * delta;
+
         // Cap at 0, split
-        if(this.energy < 0){
+        if (this.energy < 0) {
             this.energy = 0;
-        }else if(this.energy > this.eThreshhold){
-            split();
+        } else if (this.energy > this.eThreshhold) {
+            this.split();
         }
 
-        if(isParent){
-            if(children[0] != null){
-                children[0].update(delta);
-            }
-            if(children[1] != null){
-                children[1].update(delta);
+        // Adjust visual size
+        this.adjustSize();
+
+        ctx.fillRect(this.position[0] - this.size / 2.0, this.position[1] - this.size / 2.0, this.size, this.size);
+
+        if (this.isParent) {
+        if (this.children?.length > 0) {
+                this.children[0].update(delta, ctx);
+                this.children[1].update(delta, ctx);
             }
         }
-    
-        // Adjust visual size
-        adjustSize();
+
     }
-    
+
     // Handles sound input to the particle
     inputSound = (input) => {
+        if(this.children.length > 0){
+            this.children[0]?.inputSound(input);
+            this.children[1]?.inputSound(input);
+        }
         let pitch = input.pitch;
         let volume = input.volume;
-    
-        let inputMulti = getMultiplierFromPitch(pitch);
-    
+
+        let inputMulti = this.getMultiplierFromPitch(pitch);
+
         this.energyPerSecond = (volume * this.volumeMultiplier) * inputMulti;
     }
-    
+
     split = () => {
-        if(this.isParent){
+        if (this.isParent) {
+            console.log(this);
             let adjustedVariables = this.initialVariables;
             adjustedVariables.isParent = false;
-            children[0] = new Particle(adjustedVariables);
-            children[1] = new Particle(adjustedVariables);
-        }else{
-            
+            adjustedVariables.sizeMult = 1;
+            adjustedVariables.position[0] += (Math.random()-0.5) * 20;
+            adjustedVariables.position[1] += (Math.random()-0.5) * 20; 
+            this.children[0] = new Particle(adjustedVariables);
+            this.children[1] = new Particle(adjustedVariables);
+            this.energy = 0;
+        } else {
+
         }
         return;
     }
-    
+
     // Returns a multiplier based on a given pitch (0 - 1.0)
     getMultiplierFromPitch = (pitch) => {
         let pDif = Math.abs(this.desiredPitch - pitch);
-        if(pDif > this.pitchVariance){
+        if (pDif > this.pitchVariance) {
             return 0;
-        }else{
-            return (this.pitchVariance - pDif)/this.pitchVariance;
+        } else {
+            return (this.pitchVariance - pDif) / this.pitchVariance;
         }
     }
-    
+
     // Adjusts size based on current energy
     adjustSize = () => {
-        size = 1.0 + (sizeMult * energy);
+        this.size = 1.0 + (this.sizeMult * this.energy);
     }
-    
-    
+
+
     // Normalizes wander
     normalizeWander = () => {
         let mag = this.wander[0] * this.wander[0] + this.wander[1] * this.wander[1];
-        this.wander[0] = this.wander[0]/mag;
-        this.wander[1] = this.wander[1]/mag;
+        this.wander[0] = this.wander[0] / mag;
+        this.wander[1] = this.wander[1] / mag;
     }
-    
+
     // Moves forwards
-    moveForward = (delta) => {
-        this.position[0] = this.position[0] + (this.wander[0] * delta * this.speed);
-        this.position[1] = this.position[1] + (this.wander[1] * delta * this.speed); 
+    moveForward = (delta = 1.0 / 60.0) => {
+        this.position[0] = this.position[0] + (this.wander[0] * delta * 1);
+        this.position[1] = this.position[1] + (this.wander[1] * delta * 1);
     }
 
     // Getters
@@ -122,4 +142,4 @@ class Particle{
     }
 }
 
-export {Particle};
+export { Particle };
